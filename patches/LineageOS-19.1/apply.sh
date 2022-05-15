846ebb65ee9cc1b8f9491b6ad92eaba826e70d67
Giovanni Ricca <giovanniricca@protonmail.com>
2022-05-15 16:17:35 +0200

    fixup! local_manifest: Add scripts to apply patches automatically
    Change-Id: I0b8bf702ad6e0f5010ec8e93d1fe5e8ce16714cc

--- patches/LineageOS-19.1/apply.sh
+++ patches/LineageOS-19.1/apply.sh
@@ -3,15 +3,15 @@
 # .repo/local_manifests/patches
 
 # frameworks
-cp frameworks/base/0001-Add-support-for-app-signature-spoofing.patch ../../../frameworks/base/
-cd ../../../frameworks/base/
+cp frameworks/base/0001-Add-support-for-app-signature-spoofing.patch ../../../../frameworks/base/
+cd ../../../../frameworks/base/
 git am 0001-Add-support-for-app-signature-spoofing.patch
-cd ../../.repo/local_manifests/patches
+cd ../../.repo/local_manifests/patches/LineageOS-19.1
 
 # packages
-cp packages/modules/Permission/0001-Utils-add-FAKE_PACKAGE_SIGNATURE-to-platform-permiss.patch ../../../packages/modules/Permission
-cp packages/apps/Updater/0001-updater-Use-gh-releases-to-host-18-otas.patch ../../../packages/apps/Updater/
-cd ../../../packages/apps/Updater/
-git am 0001-updater-Use-gh-releases-to-host-18-otas.patch
+cp packages/modules/Permission/0001-Utils-add-FAKE_PACKAGE_SIGNATURE-to-platform-permiss.patch ../../../../packages/modules/Permission
+cp packages/apps/Updater/0001-updater-Use-gh-releases-to-host-19-otas.patch ../../../../packages/apps/Updater/
+cd ../../../../packages/apps/Updater/
+git am 0001-updater-Use-gh-releases-to-host-19-otas.patch
 cd ../../modules/Permission/
 git am 0001-Utils-add-FAKE_PACKAGE_SIGNATURE-to-platform-permiss.patch

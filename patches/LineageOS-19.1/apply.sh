59b055556fbf83a0a25df5bfa3968f3c01ed130d
Giovanni Ricca <giovanniricca@proton.me>
2022-07-09 11:15:42 +0200

    lineage-19.1: Sync vendor/extra
    Change-Id: I98949445516dfafd2e3e8740ae56d6763b7c8d70

--- patches/LineageOS-19.1/apply.sh
+++ patches/LineageOS-19.1/apply.sh
@@ -12,8 +12,5 @@ cd ../../.repo/local_manifests/patches/LineageOS-19.1
 
 # packages
 cp packages/modules/Permission/0001-Utils-add-FAKE_PACKAGE_SIGNATURE-to-platform-permiss.patch ../../../../packages/modules/Permission
-cp packages/apps/Updater/0001-updater-Use-gh-releases-to-host-19-otas.patch ../../../../packages/apps/Updater/
-cd ../../../../packages/apps/Updater/
-git am 0001-updater-Use-gh-releases-to-host-19-otas.patch
 cd ../../modules/Permission/
 git am 0001-Utils-add-FAKE_PACKAGE_SIGNATURE-to-platform-permiss.patch

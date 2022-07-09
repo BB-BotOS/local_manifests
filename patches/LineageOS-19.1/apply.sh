40836236eb7b29bba21481be7ab740bebdc3d110
Giovanni Ricca <giovanniricca@proton.me>
2022-07-09 23:38:21 +0200

    local_manifest: lineage-19.1: FIxup apply script
    Change-Id: Ifd45de0ff3e3d95b8c73d6e79371f7362085bd52

--- patches/LineageOS-19.1/apply.sh
+++ patches/LineageOS-19.1/apply.sh
@@ -12,5 +12,6 @@ cd ../../.repo/local_manifests/patches/LineageOS-19.1
 
 # packages
 cp packages/modules/Permission/0001-Utils-add-FAKE_PACKAGE_SIGNATURE-to-platform-permiss.patch ../../../../packages/modules/Permission
-cd ../../modules/Permission/
+cd ../../../../packages/modules/Permission
 git am 0001-Utils-add-FAKE_PACKAGE_SIGNATURE-to-platform-permiss.patch
+cd ../../.repo/local_manifests/patches/LineageOS-19.1

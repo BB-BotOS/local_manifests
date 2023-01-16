2cc7f7b0a2f419e0c71e9e7cb8dfca6ffcd599fe
Giovanni Ricca <giovanniricca@proton.me>
2023-01-16 08:51:25 +0100

    local_manifests: Add a cool script
    Change-Id: If80efa1de09a6b7b0e9151d77f47381a2a1e0777

--- build_release.sh
+++ build_release.sh
@@ -1,24 +1,32 @@
-# local_manifests
+#!/bin/bash
 
-## Personal local manifest for my LineageOS builds
+# Build script for my build "for personal usage only"
 
-> :warning: I won't give support to **buildbot** LineageOS. **Be very careful!**
+devices=(
+    "daisy"
+    "ysl"
+    # "lisa"
+)
 
-```bash
 ## Sync
-LOS_VERSION=20.0 # 19.1 , 20.0
-LOS_VERSION_SHORT=20 # 19 , 20
+LOS_VERSION=20.0
+LOS_VERSION_SHORT=20
 # Init LineageOS repos
 repo init -u https://github.com/LineageOS/android.git --depth=1 -b lineage-${LOS_VERSION}
 # Add local_manifests
 rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
 curl https://gitlab.com/itsvixano-dev/local_manifests/-/raw/main/lineage-${LOS_VERSION_SHORT}.xml -o .repo/local_manifests/lineage.xml
+curl https://gitlab.com/itsvixano-dev/local_manifests/-/raw/main/extra.xml -o .repo/local_manifests/extra.xml
 # Sync
 repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 
 ## Build
-. build/envsetup.sh
-lunch lineage_lisa-userdebug # lisa, daisy, ysl
-mka bacon 
-```
+# Init envsetup
+. vendor/extra/build/envsetup.sh -p
+# Build
+for device in ${devices[@]}; do
+    echo "Build for ${device}"
+    sleep 3
+    mka_build ${device} -r
+done

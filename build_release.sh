5390aa3fe9d6611fdaaa68e6f55a7ab8855ca4ed
MíkúX <121728326+MikuX-Dev@users.noreply.github.com>
2023-05-19 12:15:28 +0530

    Update build_release.sh
    Signed-off-by: MíkúX <121728326+MikuX-Dev@users.noreply.github.com>

--- build_release.sh
+++ build_release.sh
@@ -2,36 +2,42 @@
 
 # Build script for my build "for personal usage only"
 
-devices=(
-    "lisa"
-    "daisy"
-    "ysl"
-)
+#devices=(
+#    "lisa"
+#    "daisy"
+#    "ysl"
+#)
 
 ## Sync
-LOS_VERSION=20.0
-LOS_VERSION_SHORT=20
+#EOS_VERSION=v1-t
+#EOS_VERSION_SHORT=v1-t
+
 # Init LineageOS repos
-repo init -u https://github.com/LineageOS/android.git --depth=1 -b lineage-${LOS_VERSION} --git-lfs
+repo init -u https://gitlab.e.foundation/e/os/releases.git --depth=1 -b v1-t
 # Resync webview
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+#rm -rf external/chromium-webview/prebuilt/*
+#rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+#rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+
 # Add local_manifests
 rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
-curl https://gitlab.com/itsvixano-dev/local_manifests/-/raw/main/lineage-${LOS_VERSION_SHORT}.xml -o .repo/local_manifests/lineage.xml
-curl https://gitlab.com/itsvixano-dev/local_manifests/-/raw/main/extra.xml -o .repo/local_manifests/extra.xml
+curl https://github.com/BB-BotOS/local_manifests/-/raw/main/eos-v1-t.xml -o .repo/local_manifests/lineage.xml
+#curl https://github.com/BB-BotOS/local_manifests/-/raw/main/extra.xml -o .repo/local_manifests/extra.xml
+
 # Sync
 repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
-repo forall external/chromium-webview/prebuilt/* -c "git lfs pull"
+#repo forall external/chromium-webview/prebuilt/* -c "git lfs pull"
 
 ## Build
 # Init envsetup
 . vendor/extra/build/envsetup.sh -p
+
 # Build
-for device in ${devices[@]}; do
-    LOGI "Build for ${device}"
-    sleep 3
-    mka_build --device ${device} -r
-done
+#for device in ${devices[@]}; do
+#    LOGI "Build for ${device}"
+#    sleep 3
+#    mka_build --device ${device} -r
+#done
+
+
